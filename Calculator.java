1098cd32ebcc3278521019ff0c7e995634213372
akosua.adin <Dobee@Akosuas-MacBook-Air.local>
2017-01-23 15:27:45 +0000

    Divide function successfully implemented

--- Calculator.java
+++ Calculator.java
@@ -1,35 +1,37 @@
 public class Calculator{
-	
-	public static void main(String param[]) {
-		
-		
-		System.out.println("Enter The name of an Operator followed by two number delimited by a space");
-		
-		// add method
-		
-		if(param[0].equals("add")){
-			System.out.println(Integer.parseInt(param[1]) + Integer.parseInt(param[2]) );
-			
-		}
-		
+ 
+ public static void main(String param[]) {
+  
+  
+  System.out.println("Enter The name of an Operator followed by two number delimited by a space");
+  
+  // add method
+  
+  if(param[0].equals("add")){
+   System.out.println(Integer.parseInt(param[1]) + Integer.parseInt(param[2]) );
+   
+  }
+  
 
-		// subtract method
-		if(param[0].equals("sub")){
-			System.out.println(Integer.parseInt(param[1]) - Integer.parseInt(param[2]) );
-			
-		}
+  // subtract method
+  if(param[0].equals("sub")){
+   System.out.println(Integer.parseInt(param[1]) - Integer.parseInt(param[2]) );
+   
+  }
 
-		// multiply method
-		
-		if(param[0].equals("mult")){
-			System.out.println(Integer.parseInt(param[1]) * Integer.parseInt(param[2]) );
-			
-		}
-		
-		
-		
-	}
-	
-	
-	
+  // multiply method
+  
+  if(param[0].equals("mult")){
+   System.out.println(Integer.parseInt(param[1]) * Integer.parseInt(param[2]) );
+   
+  }
+  
+  //divide method
+  if(param[0].equals("div")){
+   System.out.println(Integer.parseInt(param[1]) / Integer.parseInt(param[2]) );
+  
+ }
+ }
+ 
+ 
 }

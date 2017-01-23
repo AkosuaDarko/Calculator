1502fbf7dbe6796ebb805ba298f4eb2a2aed7c11
PagesCoffy <isaac.p.coffie@gmail.com>
2017-01-23 15:03:22 +0000

    Add method successfully commited

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator{
+	
+	public static void main(String param[]) {
+		
+		
+		System.out.println("Enter The name of an Operator followed by two number delimited by a space");
+		
+		// add method
+		
+		if(param[0].equals("add")){
+			System.out.println(Integer.parseInt(param[1]) + Integer.parseInt(param[2]) );
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
